404e173dbeace46f5b30b33166127caa831db26b
shreyail <37689459+shreyail@users.noreply.github.com>
2023-05-11 09:37:56 +0000

    Update AzureSpringAppApplication.java

--- src/main/java/com/ms/gtr/poc/azureSpringApp/AzureSpringAppApplication.java
+++ src/main/java/com/ms/gtr/poc/azureSpringApp/AzureSpringAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AzureSpringAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Running AzureSpringAppApplication..........");
 		SpringApplication.run(AzureSpringAppApplication.class, args);
 	}
 
